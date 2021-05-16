1071563ac272bb454558de902523e9526713dd6b
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-05-16 11:29:24 +0200

    Classwork
    Classwork exercises done until exercise F

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from 'react';
 function App() {
+  console.log('Hi');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
 }
-
-export default App;
+let Logo = () => {
+  let appName = 'Shawen Pokedex';
+  return (
+    <header>
+      <h1>Welcome to the {appName}</h1>
+      <img
+        src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png'
+        alt='Sparrow'
+      />
+    </header>
+  );
+};
+let BestPokemon = () => <p>My favorite Pokemon is Squirtle</p>;
+let CaughtPokemon = () => {
+  let date = new Date().toLocaleDateString();
+  let abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
+  return (
+    <div>
+      <p>Caught 0 Pokemon on {date}</p>
+      <ul>
+        {abilities.map((ability, index) => (
+          <li key = {index}>{ability}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
