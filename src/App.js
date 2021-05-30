e35f47c4ad595e4a69c46aad3a7e7a3ab6f6f158
ShawenHarker <harkershawen@gmail.com>
2021-05-30 09:07:35 +0200

    completed class course work until excerise D

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Logo from "./logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+import PokemonMovesSelector from "./PokemonMovesSelector";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Logo appName="Shawen's Pokedex" handleClick={logWhenClicked} />
-      <BestPokemon abilities={["Anticipation", "Adaptability", "Fly-Away"]}/>
-      <CaughtPokemon date = {new Date().toLocaleDateString()}/>
+      <BestPokemon abilities={["Anticipation", "Adaptability", "Fly-Away"]} />
+      <CaughtPokemon date={new Date().toLocaleDateString()} />
+      <PokemonMovesSelector />
     </div>
   );
 }
---
+++ src/PokemonMoves.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+function PokemonMoves(props) {
+    let [pokemonData, setPokemonData] = useState(null);
+    useEffect (() => {
+        fetch(
+            `https://pokeapi.co/api/v2/pokemon/${props.pokemonId}/`        )
+        .then((res) => res.json())
+        .then((data) => {
+            setPokemonData(data);
+        });
+    }, [props.pokemonId]);
+
+    return (pokemonData) ? (
+        <div>
+            <p>{pokemonData.name}'s moves:</p>
+            <ul>
+                {pokemonData.moves.map((move, index) => {
+                return <li key={index}>{move.move.name}</li>;
+                })}
+            </ul>
+        </div>
+    ) : null;
+}
+
+export default PokemonMoves;
